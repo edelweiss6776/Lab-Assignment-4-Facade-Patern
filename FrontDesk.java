6fc07a4e4f673c0c4a28cdd478f18e508495f7e0
edelweiss6776 <ramosjairus6776@gmail.com>
2024-10-07 23:06:18 +0800

    Create FrontDesk.java

---
+++ FrontDesk.java
@@ -0,0 +1,24 @@
+public class FrontDesk {
+    
+    private Valet valet;
+    private HouseKeeping houseKeeping;
+    private Cart cart;
+    
+    public FrontDesk() {
+        valet = new Valet();
+        houseKeeping = new HouseKeeping();
+        cart = new Cart();
+    }
+    
+    public void requestValet(String plateNumber) {
+        valet.pickUpVehicle(plateNumber);
+    }
+    
+    public void requestHouseKeeping(int roomNumber) {
+        houseKeeping.cleanRoom(roomNumber);
+    }
+    
+    public void requestCart(int numberOfCarts) {
+        cart.requestCart(numberOfCarts);
+    }
+}
